ec039e77b9ec5a4322fd196ca5ce15907d367cb5
aditya14011999 <adipathak786@gmail.com>
2020-12-30 18:28:51 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONNOS SHOP</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Hanalei+Fill&family=Redressed&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="header">
+    <div class="container">
+    <div class="navbar">
+    <div class="logo">
+    <img src="images/logo.png" width="125px">
+    </div>
+    <nav>
+    <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="product.php">Products</a></li>
+    <li><a href="form.php">Contact Us</a></li>
+    </ul>
+    </nav>
+    <img src="images/cart.png" width="30px" height="30px">
+    </div>
+<div class="row">
+    <div class="col-2">
+        <h1> GIFT YOUR LOVE ONES<br> A SURPRISE</h1>
+        <p>Lorem Ipsum is simply dummy text <br> the printing and typesetting industry. </p>
+        <a href="" class="btn" >Explore Now &#8594;</a>
+</div>
+
+        <div class="col-2 img">
+        <img src="images/image1.png">
+        </div>
+    
+</div>
+ </div>
+ </div>
+
+ <div class="categories">
+     <div class="small">
+     <div class="row">
+         <div class="col-3">
+             <img src="images/category-1.jpg" alt="">
+
+         </div>
+         <div class="col-3">
+         <img src="images/category-2.jpg" alt="">
+             
+             </div>
+             <div class="col-3">
+             <img src="images/category-3.jpg" alt="">
+             
+             </div>
+     </div>
+ </div>
+
+     </div>
+
+
+
+     <div class="small">
+         <h2 class="title">Feautered Products</h2>
+         <div class="row">
+             <div class="col-4">
+                 <img src="images/product-1.jpg" >
+                 <h4>Red T-shirt</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+             <div class="col-4">
+                 <img src="images/product-2.jpg" >
+                 <h4>Shoes</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+             <div class="col-4">
+                 <img src="images/product-3.jpg" >
+                 <h4>Joggers</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+         </div>
+     </div>
+
+
+     <div class="brands">
+     <h2 class="title">Our Brands</h2>
+         <div class="small">
+             <div class="row">
+                 <div class="col-5">
+                     <img src="images/logo-godrej.png" alt="">
+                 </div>
+                 <div class="col-5">
+                     <img src="images/logo-oppo.png" alt="">
+                 </div>
+                 <div class="col-5">
+                     <img src="images/logo-coca-cola.png" alt="">
+                 </div>
+                 <div class="col-5">
+                     <img src="images/logo-paypal.png" alt="">
+                 </div>
+                 <div class="col-5">
+                     <img src="images/logo-philips.png" alt="">
+                 </div>
+             </div>
+         </div>
+     </div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Download our app</h3>
+                <p>Download app for android and ios</p>
+                <div class="app-logo">
+                    <img src="images/play-store.png" alt="">
+                    <img src="images/app-store.png" alt="">
+                </div>
+            </div>
+            <div class="footer-col-2">
+               <img src="images/logo-white.png" alt="">
+                <p>Our motto is customers happineess</p>
+            </div>
+            <div class="footer-col-3">
+            <h3>Useful links </h3>
+                <ul>
+                    <li>coupons</li>
+                    <li>Blog Post</li>
+                    <li>Return Policy</li>
+                    <li>Join Us</li>
+                </ul>
+            </div>
+            <div class="footer-col-4">
+            <h3>Useful links </h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Youtube</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
+     
+    
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<body>
+    <div class="header">
+    <div class="container">
+    <div class="navbar">
+    <div class="logo">
+    <img src="images/logo.png" width="125px">
+    </div>
+    <nav>
+    <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="product.php">Products</a></li>
+    <li><a href="form.php">Contact Us</a></li>
+    </ul>
+    </nav>
+    <img src="images/cart.png" width="30px" height="30px">
+    </div>
+
+    <div class="categories">
+     <div class="small">
+     <div class="row">
+         <div class="col-3">                                                    
+             <img src="images/category-1.jpg" alt="">
+
+         </div>
+         <div class="col-3">
+         <img src="images/category-2.jpg" alt="">
+             
+             </div>
+             <div class="col-3">
+             <img src="images/category-3.jpg" alt="">
+             
+             </div>
+     </div>
+ </div>
+
+     </div>
+
+
+
+     <div class="small">
+         <h2 class="title">Feautered Products</h2>
+         <div class="row">
+             <div class="col-4">
+                 <img src="images/product-1.jpg" >
+                 <h4>Red T-shirt</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+             <div class="col-4">
+                 <img src="images/product-2.jpg" >
+                 <h4>Shoes</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+             <div class="col-4">
+                 <img src="images/product-3.jpg" >
+                 <h4>Joggers</h4>
+                 <div class="rating">
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star" aria-hidden="true"></i>
+                 <i class="fa fa-star-o" aria-hidden="true"></i>
+                 </div>
+                 <p>Rs150</p>
+             </div>
+         </div>
+     </div>
+
+
+     <div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Download our app</h3>
+                <p>Download app for android and ios</p>
+                <div class="app-logo">
+                    <img src="images/play-store.png" alt="">
+                    <img src="images/app-store.png" alt="">
+                </div>
+            </div>
+            <div class="footer-col-2">
+               <img src="images/logo-white.png" alt="">
+                <p>Our motto is customers happineess</p>
+            </div>
+            <div class="footer-col-3">
+            <h3>Useful links </h3>
+                <ul>
+                    <li>coupons</li>
+                    <li>Blog Post</li>
+                    <li>Return Policy</li>
+                    <li>Join Us</li>
+                </ul>
+            </div>
+            <div class="footer-col-4">
+            <h3>Useful links </h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Youtube</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
